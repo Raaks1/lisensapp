d9bf98515f5ac4bbe5e9f882a380dd28dbdda018
Raaks1 <88654732+Raaks1@users.noreply.github.com>
2022-06-20 13:13:14 +0200

    config update

--- src/Config.ts
+++ src/Config.ts
@@ -1,7 +1,7 @@
 const config = {
- /*appId: '101b8ac5-ff79-4528-926f-13683c0196e4',*/
-   appId: 'cb854d0b-f426-480f-8e6d-6bfba838b6b4',
-   
+ appId: '101b8ac5-ff79-4528-926f-13683c0196e4',
+  /* appId: 'cb854d0b-f426-480f-8e6d-6bfba838b6b4',*/
+  
     
   /*  tenantID:'653549de-3049-4012-8bff-4dea40ec7ac7',*/
  /* authority: 'https://login.microsoftonline.com/lisensid.tofsandbox.onmicrosoft.com',*/
